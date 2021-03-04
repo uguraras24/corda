8685c80e5c426e615a76a6a506724845ab574079
uguraras24 <65421324+uguraras24@users.noreply.github.com>
2021-03-04 16:56:27 +0300

    ViewInboxByAccount

--- Accounts/supplychain/workflows/src/main/java/net/corda/samples/supplychain/accountUtilities/ViewInboxByAccount.java
+++ Accounts/supplychain/workflows/src/main/java/net/corda/samples/supplychain/accountUtilities/ViewInboxByAccount.java
@@ -9,14 +9,14 @@ import net.corda.core.flows.StartableByRPC;
 import net.corda.core.flows.StartableByService;
 import net.corda.core.node.services.vault.QueryCriteria;
 import net.corda.samples.supplychain.states.*;
-
+import net.corda.core.contracts.StateAndRef;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import org.json.simple.*;
 
 @StartableByRPC
 @StartableByService
-public class ViewInboxByAccount extends FlowLogic<List<String>>{
+public class ViewInboxByAccount extends FlowLogic<List<String>> {
 
     private final String acctName;
 
@@ -27,24 +27,36 @@ public class ViewInboxByAccount extends FlowLogic<List<String>>{
     @Override
     public List<String> call() throws FlowException {
 
+        List<String> flowlist = new ArrayList<String>();
+
+        List<String> payments = new ArrayList<String>();
+        List<String> month = new ArrayList<String>();
+        List<String> distribution = new ArrayList<String>();
+        List<String> fund = new ArrayList<String>();
+
         AccountService accountService = getServiceHub().cordaService(KeyManagementBackedAccountService.class);
         AccountInfo myAccount = accountService.accountInfo(acctName).get(0).getState().getData();
+
         QueryCriteria.VaultQueryCriteria criteria = new QueryCriteria.VaultQueryCriteria()
                 .withExternalIds(Arrays.asList(myAccount.getIdentifier().getId()));
 
-        List<String> InternalMessages = getServiceHub().getVaultService().queryBy(InternalMessageState.class,criteria).getStates().stream().map(
-                it -> "\nInternalMessages State : " + it.getState().getData().getTask()).collect(Collectors.toList());
+        payments = getServiceHub().getVaultService().queryBy(PaymentState.class, criteria).getStates().stream().map(
+                it -> "" + it.getState().getData().getAmount()).collect(Collectors.toList());
+        month = getServiceHub().getVaultService().queryBy(PaymentState.class, criteria).getStates().stream().map(
+                it -> "" + it.getState().getData().getMonth()).collect(Collectors.toList());
+        distribution = getServiceHub().getVaultService().queryBy(PaymentState.class, criteria).getStates().stream().map(
+                it -> "" + it.getState().getData().getDistribution()).collect(Collectors.toList());
+        fund = getServiceHub().getVaultService().queryBy(PaymentState.class, criteria).getStates().stream().map(
+                it -> "" + it.getState().getData().getFundid()).collect(Collectors.toList());
 
-        List<String> payments = getServiceHub().getVaultService().queryBy(PaymentState.class,criteria).getStates().stream().map(
-                it -> "\nPayment : " +it.getState().getData().getAmount()).collect(Collectors.toList());
-        List<String> month = getServiceHub().getVaultService().queryBy(PaymentState.class,criteria).getStates().stream().map(
-                it -> "\nMonth : " +it.getState().getData().getMonth()).collect(Collectors.toList());
-        List<String> distribution = getServiceHub().getVaultService().queryBy(PaymentState.class,criteria).getStates().stream().map(
-                it -> "\nDistribution : " +it.getState().getData().getDistribution()).collect(Collectors.toList());
-        List<String> fund = getServiceHub().getVaultService().queryBy(PaymentState.class,criteria).getStates().stream().map(
-                it -> "\nFund : " +it.getState().getData().getFundid()).collect(Collectors.toList());
-
-
-        return Stream.of(InternalMessages, payments,month,distribution,fund).flatMap(Collection::stream).collect(Collectors.toList());
+        for(int i=0;i<payments.size();i++){
+            JSONObject flow = new JSONObject();
+            flow.put("payment",payments.get(i));
+            flow.put("month",month.get(i));
+            flow.put("distribution",distribution.get(i));
+            flow.put("fundid",fund.get(i));
+            flowlist.add(flow.toString());
+        }
+        return flowlist;
     }
 }
